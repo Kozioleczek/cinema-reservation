a82032643a1c47036844d88f6a6f3f734d303485
DawidNezo <nezo.dm@gmail.com>
2022-05-26 22:34:51 +0200

    swagger

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -7,6 +7,29 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Cinema API documentation",
+ *      description="Swagger documentation",
+ *      @OA\Contact(
+ *          name="Dawid Mazur"
+ *      ),
+ *      @OA\Contact(
+ *          name="Wojciech Kozioł"
+ *      )
+ * )
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Server"
+ * )
+ *
+ * @OA\ExternalDocumentation(
+ *     description="Find out more about Swagger",
+ *     url="http://swagger.io"
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
